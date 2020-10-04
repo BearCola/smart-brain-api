927599a0382512afac78f0575ffdb47710f08ab7
sangin <sangin84@gmail.com>
2020-10-05 06:29:03 +0900

    update

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const knex = require("knex");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "sangin",
+    host: "postgresql-cubic-85500",
+    user: "diablo1031",
     password: "",
     database: "smart-brain",
   },
